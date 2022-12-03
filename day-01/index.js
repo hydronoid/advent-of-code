b257664eadb170c69d8278459bedefbab0e387e4
Alan Zhao <alanzhao.02@gmail.com>
2022-12-03 14:56:47 +0800

    improvements to day-01

--- day-01/index.js
+++ day-01/index.js
@@ -5,42 +5,17 @@ function inputArray(filename) {
     return data.split(/\r?\n/).map(Number);
 }
 
-function part1(filename) {
-    let arr = inputArray(filename);
-    let maxCalories = Number.MIN_SAFE_INTEGER;
-    let currentCalories = 0;
-    for (let i = 0; i < arr.length; i++) {
-        
-        
-        if (arr[i] == 0) currentCalories = 0;
-        currentCalories += arr[i];
-        if (currentCalories > maxCalories) {
-            maxCalories = currentCalories;
-        }
+let arr = inputArray('input.txt');
+let elfArr = [];
+let currentCalories = 0;
+for (let i = 0; i < arr.length; i++) {
+    currentCalories += arr[i];
+    if (arr[i] == 0)  {
+        elfArr.push(currentCalories);
+        currentCalories = 0;
     }
-    return maxCalories;
 }
+elfArr.sort((a, b) => b - a);
 
-function part2(filename) {
-    let arr = inputArray(filename);
-    let elfArr = [];
-    let currentCalories = 0;
-    for (let i = 0; i < arr.length; i++) {
-        currentCalories += arr[i];
-        if (arr[i] == 0)  {
-            elfArr.push(currentCalories);
-            currentCalories = 0;
-        }
-    }
-    elfArr.push(currentCalories);
-    elfArr.sort((a, b) => b - a);
-    return elfArr[0] + elfArr[1] + elfArr[2];
-
-}
-
-console.log(part1('sample.txt'));
-console.log(part2('sample.txt'));
-
-
-console.log(part1('input.txt'));
-console.log(part2('input.txt'));
+console.log("Part 1: ", elfArr[0]);
+console.log("Part 2: ", elfArr[0] + elfArr[1] + elfArr[2]);
