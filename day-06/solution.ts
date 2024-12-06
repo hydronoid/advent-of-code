4f53dccf6594690786f254bae788cf3d344eef48
Alan Zhao <alanzhao.02@gmail.com>
2024-12-06 15:53:46 +0800

    day 6 improvements

--- day-06/solution.ts
+++ day-06/solution.ts
@@ -1,10 +1,8 @@
 import { readInputToArray } from '../input-utils';
 
 let grid: string[] = readInputToArray('input.txt');
-
 let startPos: number[] = [-1, -1];
 let directions: number[][] = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-let visitedP1: { [key: string]: boolean } = {};
 
 // find the starting position '^'
 for (let i = 0; i < grid.length; i++)
@@ -12,54 +10,52 @@ for (let i = 0; i < grid.length; i++)
         if (grid[i][j] == '^')
             startPos = [i, j];
 
-function traverse(pos: number[], visited: { [key: string] : boolean }, newObstructionPos: number[] = [-1, -1]): boolean {
+function traverse(pos: number[], visited: { [key: string] : number[] }, newObstructionPos: number[] = [-1, -1]): boolean {
     let currentDirection = directions[0]; // start off going upwards
     let dirIdx = 0;
     let [i, j] = pos;
     let [iObstruction, jObstruction] = newObstructionPos; 
-
-    // part 2: keep track of the position whenever we hit an obstruction
-    // in order to detect a loop, we check if it's the same pos as the last time we hit it
-    // key: position of the obstruction, value: position where we bump into it
-    let obstructionHistory: { [key: string]: number[] } = {};
 
     while (true) {
         let new_i: number = i + currentDirection[0]
         let new_j: number = j + currentDirection[1];
         let currentPosKey: string = JSON.stringify([i, j]);
 
+        // reached outside the grid -> finished traversing
         if (new_i < 0 || new_i >= grid.length || new_j < 0 || new_j >= grid[0].length) {
-            visited[currentPosKey] = true;
+            visited[currentPosKey] = currentDirection;
             break;
         }
         
+        // reached the current position coming from the same direction as the last time we reached it -> loop found
+        if (visited[currentPosKey]?.[0] == currentDirection[0] && visited[currentPosKey]?.[1] == currentDirection[1])
+            return true;
+        
         if (grid[new_i][new_j] === '#' || new_i === iObstruction && new_j === jObstruction) {
-            // part 2: check the last position when hitting this obstacle
-            let obstructionPosKey: string = JSON.stringify([new_i, new_j]);
-            if (obstructionHistory[obstructionPosKey]?.[0] === i && obstructionHistory[obstructionPosKey]?.[1] === j)
-                return true;
-            
-            obstructionHistory[obstructionPosKey] = [i, j];
-            
             // rotate direction 90 degrees, i.e. the next element in the directions array
             currentDirection = directions[++dirIdx % directions.length];
             continue;
         }
-        visited[currentPosKey] = true;
+        visited[currentPosKey] = currentDirection;
         i = new_i;
         j = new_j;
     }
+    // no loop found
     return false;
 }
 
+// Part 1
+let visitedP1: { [key: string]: number[] } = {}; // key: position, value: direction
 traverse(startPos, visitedP1);
 
+// Part 2
 let loops = 0;
 
 // use visited positions from part 1 as the possible obstruction positions
+// and then brute force these positions
 for (let pos of Object.keys(visitedP1)) {
     let newObsPos: number[] = JSON.parse(pos);
-    let visitedP2: { [key: string]: boolean } = {};
+    let visitedP2: { [key: string]: number[] } = {};
     if (traverse(startPos, visitedP2, newObsPos))
         loops++;
 }
