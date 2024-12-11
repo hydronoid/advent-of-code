f587d7dabdf072372a63f2a7099c21149ad3d047
Alan Zhao <alanzhao.02@gmail.com>
2024-12-11 14:36:56 +0800

    day 11 solution

--- day-11/solution.ts
+++ day-11/solution.ts
@@ -1,9 +1,7 @@
 import { readInputToString } from '../input-utils';
 
-let arr: number[] = readInputToString('input.txt').split(' ').map(Number);
-
-function blink(stone: number): number[] {
-    if  (stone === 0) {
+function changeStone(stone: number): number[] {
+    if (stone === 0) {
         return [1];
     } else if (stone.toString().length % 2 === 0) {
         const numStr = stone.toString();
@@ -13,10 +11,29 @@ function blink(stone: number): number[] {
     return [stone * 2024];
 }
 
-for (let i = 0; i < 25; i++) {
-    let newArr: number[] = [];
-    arr.forEach((stone) => newArr.push(...blink(stone)));
-    arr = newArr;
-}
+function blink(numBlinks: number): number {
+    // key: stone number, value: amount of stones (initially 1)
+    let counter = new Map<number, number>(readInputToString('input.txt')
+        .split(' ')
+        .map(x => [parseInt(x), 1])
+    );
 
-console.log(arr.length);
+    for (let _ = 0; _ < numBlinks; _++) {
+        // make a new map to avoid issues with changing the original map during iteration
+        let newCounter = new Map<number, number>();
+        counter.forEach((count, stone) => {
+            // for each new stone n, its count is accumulated by the amount of stones of its previous state
+            const newStones: number[] = changeStone(stone);
+            for (const n of newStones)
+                newCounter.set(n, (newCounter.get(n) || 0) + count);
+        });
+        counter = newCounter;
+    }
+    
+    let numStones = 0;
+    counter.forEach((count) => numStones += count);
+    return numStones;
+}    
+
+console.log(`Part 1: ${blink(25)}`); // 202019
+console.log(`Part 2: ${blink(75)}`); // 239321955280205
