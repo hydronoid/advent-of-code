f2a9f9991248208d72a669bb800d672ebc249224
Alan Zhao <alanzhao.02@gmail.com>
2024-12-22 23:10:08 +0800

    day 21 part 2

--- day-21/solution.ts
+++ day-21/solution.ts
@@ -17,24 +17,20 @@ const DIRECTIONS: { [key: string]: [number, number] } = {
     'v': [1, 0],
     '<': [0, -1],
     '>': [0, 1],
-}
+};
 
 function isInBounds(i: number, j: number, keypad: string[]): boolean {
     return 0 <= i && i < keypad.length 
         && 0 <= j && j < keypad[0].length;
 }
 
-function filterToShortest(moveLst: string[]): string[] {
-    const shortestPathLen = Math.min(...moveLst.map(x => x.length));
-    return moveLst.filter(x => x.length === shortestPathLen)
-}
-
-function filterToBest(moveLst: string[]): string[] {
-    function sameConsecutiveMoves(moves: string) {
-        let score = 0;
-        let consecutiveCount = 1;
-        for (let i = 1; i < moves.length; i++) {
-            if (moves[i] === moves[i-1]) {
+function filterToOptimal(moveLst: string[]): string[] {
+    function calculateScore(moves: string) {
+        // score favours multiple of the same press
+        // since it means we move around less
+        let score = 0, consecutiveCount = 1;
+        for (let i = 1; i <= moves.length; i++) {
+            if (moves[i] === moves[i - 1]) {
                 consecutiveCount++;
             } else {
                 score += consecutiveCount ** 2;
@@ -42,13 +38,19 @@ function filterToBest(moveLst: string[]): string[] {
             }
         }
         return score;
-    }
-    const bestScore = Math.max(...moveLst.map(sameConsecutiveMoves));
+    };
     
-    return moveLst.filter(moves => sameConsecutiveMoves(moves) === bestScore);
+    // first we filter to the least amount of moves
+    const shortestPathLen = Math.min(...moveLst.map(x => x.length));
+    const shortestPaths = moveLst.filter(x => x.length === shortestPathLen);
+
+    // then we filter to those that have the best score 
+    // i.e. most amount of the same consecutive presses
+    const bestScore = Math.max(...shortestPaths.map(calculateScore));
+    return shortestPaths.filter(moves => calculateScore(moves) === bestScore);
 }
 
-function getPos(char: string, keypad: string[]) {
+function getPos(char: string, keypad: string[]): [number, number] {
     for (let i = 0; i < keypad.length; i++)
         for (let j = 0; j < keypad[0].length; j++)
             if (keypad[i][j] === char)
@@ -57,14 +59,14 @@ function getPos(char: string, keypad: string[]) {
     return [-1, -1]
 }
 
-function bfs(keypad: string[], start: string, end: string) {
+function bfs(keypad: string[], start: string, end: string): string[] {
     let queue = [[start, start, '']];
     let validMoves: string[] = [];
 
     while (queue.length > 0) {
         const [curr, path, moves] = queue.shift()!;
         if (curr === end) {
-            validMoves.push(moves);
+            validMoves.push(moves + 'A');
             continue;
         }
         const [i, j] = getPos(curr, keypad);
@@ -79,69 +81,106 @@ function bfs(keypad: string[], start: string, end: string) {
             }
         }
     }
-    return filterToShortest(validMoves);
+    return filterToOptimal(validMoves);
 }
 
 function optimalMoves(keypad: string[]): { [key: string]: { [key: string]: string[] } } {
     let moveMap: { [key: string]: { [key: string]: string[] } } = {};
 
+    // for a given keypad, find all optimal moves from
+    // a given button a at (i, j) to b at (i2, j2) via BFS
     for (let i = 0; i < keypad.length; i++) {
         for (let j = 0; j < keypad[0].length; j++) {
             const a = keypad[i][j];
+            if (a === '.')
+                continue;
             moveMap[a] = {};
             for (let i2 = 0; i2 < keypad.length; i2++) {
                 for (let j2 = 0; j2 < keypad[0].length; j2++) {
                     const b = keypad[i2][j2];
+                    if (b === '.')
+                        continue;
                     moveMap[a][b] = bfs(keypad, a, b);
                 }
             }
         }
     }
-    
     return moveMap;
 }
 
-function shortestSeq(code: string, numeric: boolean): string[] {
+function getDirPadSeqs(code: string): string[] {
     // start at A
     let queue: [string, number, string][] = [['', -1, 'A']];
-    const moves = numeric ? NUMPAD_MOVES : DIRPAD_MOVES;
-    const shortestSequences: string[] = [];
+    const seqs: string[] = [];
 
     while (queue.length > 0) {
         const [currPath, codeIdx, a] = queue.shift()!;
         if (codeIdx === code.length - 1) {
-            shortestSequences.push(currPath);
+            seqs.push(currPath);
             continue;
         }
         const b = code[codeIdx + 1];
-        for (const path of moves[a][b]) {
-            queue.push([currPath + path + 'A', codeIdx + 1, b]);
-        }
-
+        for (const path of NUMPAD_MOVES[a][b])
+            queue.push([currPath + path, codeIdx + 1, b]);
     }
-    return filterToBest(filterToShortest(shortestSequences));
+    return filterToOptimal(seqs);
 }
 
+function minSeqLen(a: string, b: string, keypadNum: number): number {
+    const key = a + b + keypadNum;
+    if (memo.hasOwnProperty(key))
+        return memo[key];
+
+    let minLength = Infinity;
+
+    for (const path of DIRPAD_MOVES[a][b]) {
+        let seqLen = 0;
+        if (keypadNum === 1) {
+            // base case
+            seqLen += path.length;
+        } else {
+            // start at A
+            seqLen += minSeqLen('A', path[0], keypadNum - 1);
+            for (let i = 0; i < path.length - 1; i++)
+                seqLen += minSeqLen(path[i], path[i+1], keypadNum - 1);
+        }
+        minLength = Math.min(seqLen, minLength);
+    }
+
+    memo[key] = minLength;
+    return minLength;
+}
+
+function calculateComplexity(code: string, robotKeypads: number): number {
+    let minLength: number = Infinity;
+    const seqs = getDirPadSeqs(code);
+    for (const moves of seqs) {
+        // start at A
+        let seqLen = minSeqLen('A', moves[0], robotKeypads);
+        for (let i = 0; i < moves.length - 1; i++)
+            seqLen += minSeqLen(moves[i], moves[i+1], robotKeypads);
+
+        minLength = Math.min(seqLen, minLength);
+    }
+    return minLength * parseInt(code);
+}
+
+// precompute optimal moves from A to B
+// for the directional pad and number pad
 const DIRPAD_MOVES = optimalMoves(DIRPAD);
 const NUMPAD_MOVES = optimalMoves(NUMPAD);
+let memo: { [key: string]: number } = {}; // for part 2 DFS
+let part1 = 0, part2 = 0;
 
-let part1 = 0;
-
-// takes 35 seconds XD
 for (const code of data) {
-    let minLength: number = Infinity;
-    const lv1 = shortestSeq(code, true);
-    for (const lv1Code of lv1) {
-        const lv2 = shortestSeq(lv1Code, false);
-        for (const lv2Code of lv2) {
-            const lv3 = shortestSeq(lv2Code, false);
-            minLength = Math.min(minLength, lv3[0].length);
-        }
-    }
-    // console.log(code, minLength)
-    part1 += minLength * parseInt(code);
-
+    part1 += calculateComplexity(code, 2);
+    part2 += calculateComplexity(code, 25);
 }
 
+// probably top 3 hardest puzzles I've done in AoC
+// relied HEAVILY on subreddit hints to get both part 1 and 2 working
+// because I had a lot of trouble completing it on my own.
+// again code may be quite messy (esp. variable names)
 
-console.log(part1); // 176870
+console.log(`Part 1: ${part1}`);    // 176870
+console.log(`Part 2: ${part2}`);    // 223902935165512
